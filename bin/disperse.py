b1f8c9baa33a7dc6c13fd5ea998d2651ed0e6e25
David A. Riggs <driggs@myotisoft.com>
2016-12-10 20:05:55 -0800

    Add simple script `bin/disperse.py` to disperse GUANO files by species label (move all files with the same species label into folders together)

---
+++ bin/disperse.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+Disperse files by moving them into folders by species label.
+
+usage:  disperse.py ROOTDIR
+"""
+
+# TODO: add options to copy vs move; distinguish between Manual / Auto ID
+
+import os
+import os.path
+from glob import glob
+
+import guano
+
+
+def get_species(fname):
+    """Get the species label from a GUANO file, or `None`. Prefer `Manual ID` over 'Auto ID'."""
+    try:
+        f = guano.GuanoFile(fname)
+    except ValueError:
+        return None
+
+    species = f.get('Species Manual ID', None)
+    return species if species else f.get('Species Auto ID', None)
+
+
+def disperse(rootdir):
+    """Disperse GUANO .wav files into folders by their species label."""
+    for fname in glob(os.path.join(rootdir, '*.wav')):
+        species = get_species(fname)
+        if not species:
+            print 'Skipping file without species %s .' % fname
+            continue
+        destination = os.path.join(rootdir, species)
+        if not os.path.isdir(destination):
+            print 'Creating directory %s ...' % destination
+            os.mkdir(destination)
+        new_fname = os.path.join(destination, os.path.basename(fname))
+        print '%s -> %s ...' % (fname, new_fname)
+        os.rename(fname, new_fname)
+
+
+def main():
+    """Commandline interface"""
+    import argparse
+    parser = argparse.ArgumentParser(description='Disperse files to folders by species field')
+    parser.add_argument('rootdir')
+    args = parser.parse_args()
+    disperse(args.rootdir)
+
+
+if __name__ == '__main__':
+    main()
