ff368d7e4b8be2dd399a38222ecfad9a91c257c9
David A. Riggs <driggs@myotisoft.com>
2017-04-27 07:39:55 -0700

    Add some basic unit tests (launch them with `make test`)

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ PYTHON=python2
 clean:
 	rm -rf *.pyo *.pyc *.egg-info dist
 
+test:
+	$(PYTHON) -m unittest discover -s tests
+
 dist:
 	$(PYTHON) setup.py sdist bdist_wheel
 
---
+++ tests/test_guano.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from guano import GuanoFile, wavparams
+
+
+class UnicodeTest(unittest.TestCase):
+
+    NOTE = u'¡GUANO is the 💩 !'
+    MD = u"""GUANO|Version: 1.0\nNote: %s""" % NOTE
+
+    def setUp(self):
+        pass
+
+    def test_from_string(self):
+        """Parse a GUANO metadata block containing Unicode data"""
+        g = GuanoFile.from_string(self.MD)
+        self.assertEqual(self.NOTE, g['Note'])
+
+    def test_file_roundtrip(self):
+        """Write a GUANO .WAV file containing Unicode data, re-read it and confirm value is identical"""
+        fname = 'test_guano.wav'
+
+        # write a fake .WAV file
+        g = GuanoFile.from_string(self.MD)
+        g.filename = fname
+        g.wav_params = wavparams(1, 2, 500000, 2, 'NONE', None)
+        g.wav_data = '\0\0'
+        g.write()
+
+        # read it back in
+        g2 = GuanoFile(fname)
+
+        self.assertEqual(self.NOTE, g2['Note'])
+
+
+class GeneralTest(unittest.TestCase):
+
+    MD = r"""GUANO|Version: 1.0
+    Timestamp: 2017-04-20T01:23:45-07:00
+    Note: This is a \nmultiline text note\nfor testing.
+    User|Haiku: five\nseven\nfive
+    User|Answer: 42
+    MSFT|Transect|Version: 1.0.16
+    """
+
+    def setUp(self):
+        GuanoFile.register('User', 'Answer', int)
+        GuanoFile.register('', 'Note', lambda x: x.replace('\\n', '\n'))
+        self.md = GuanoFile.from_string(self.MD)
+
+    def test_get_namespaces(self):
+        expected = {'GUANO', '', 'User', 'MSFT'}
+        namespaces = set(self.md.get_namespaces())
+        self.assertSetEqual(expected, namespaces)
+
+    def test_get_types(self):
+        self.assertEqual(42, self.md['User|Answer'])
+        self.assertEqual(42, self.md['User', 'Answer'])
+        self.assertEqual(42, self.md.get('User|Answer'))
+
+    def test_multiline(self):
+        self.assertEqual(3, len(self.md['Note'].splitlines()))
+
+
+if __name__ == '__main__':
+    unittest.main()
